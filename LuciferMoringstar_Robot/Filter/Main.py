7721a5b6c86202e166b4a5e6aa44a252668493a2
Xrsirx5 <91307602+Xrsirx5@users.noreply.github.com>
2021-10-30 11:35:53 +0530

    Update Main.py

--- LuciferMoringstar_Robot/Filter/Main.py
+++ LuciferMoringstar_Robot/Filter/Main.py
@@ -62,12 +62,12 @@ async def filter(client, message):
                 btn.append(
                     [InlineKeyboardButton(text=f"{filename}",callback_data=f"pr0fess0r_99#{file_id}")]
                     )
-        else:
-            LuciferMoringstar=await client.send_message(
-            chat_id = message.chat.id,
-            text=f"""
-Couldn't Find Movie.
-Please Check the spelling in google.😊 """,
+ else:
+            await client.send_sticker(chat_id=message.from_user.id, sticker='CAADBQADMwIAAtbcmFelnLaGAZhgBwI')
+            return
+
+        if not btn:
+            return
 
         if not btn:
             return
